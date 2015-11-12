5b6d43c519e1b03e5cad34b1a103ba34c91b0a61
Victor Sokolov <gzigzigzeo@evilmartians.com>
2015-11-12 19:47:38 +0100

    Split a little more

--- lib/resource_spec/create.rb
+++ lib/resource_spec/create.rb
@@ -1,24 +1,30 @@
 RSpec.shared_examples "POST :create" do
   describe "POST :create" do
-    context "success" do
-      let(:url_args) { create_url_args }
-      let(:redirect_url) { success_create_url }
+    include_examples "POST :create, success"
+  end
+end
 
-      before { post :create, url_args }
+RSpec.shared_examples "POST :create, success" do
+  context "success" do
+    let(:url_args) { create_url_args }
+    let(:redirect_url) { success_create_url }
 
-      include_examples "responds 301"
-      include_examples "resource has no errors"
-      include_examples "resource persisted"
-      include_examples "resource has expected attributes"
-    end
+    before { post :create, url_args }
 
-    context "fail" do
-      let(:url_args) { invalid_create_url_args }
+    include_examples "responds 301"
+    include_examples "resource has no errors"
+    include_examples "resource persisted"
+    include_examples "resource has expected attributes"
+  end
+end
 
-      before { post :create, url_args }
+RSpec.shared_examples "POST :create, fail" do
+  context "fail" do
+    let(:url_args) { invalid_create_url_args }
 
-      include_examples "responds 200"
-      include_examples "resource has errors"
-    end
+    before { post :create, url_args }
+
+    include_examples "responds 200"
+    include_examples "resource has errors"
   end
 end
--- lib/resource_spec/update.rb
+++ lib/resource_spec/update.rb
@@ -1,23 +1,30 @@
 RSpec.shared_examples "PUT :update" do
   describe "PUT :update" do
-    context "success" do
-      let(:url_args) { update_url_args }
-      let(:redirect_url) { success_update_url }
+    include_examples "PUT :update, success"
+    include_examples "PUT :update, fail"
+  end
+end
 
-      before { put :update, url_args }
+RSpec.shared_examples "PUT :update, success" do
+  context "success" do
+    let(:url_args) { update_url_args }
+    let(:redirect_url) { success_update_url }
 
-      include_examples "responds 301"
-      include_examples "resource has no errors"
-      include_examples "resource has expected attributes"
-    end
+    before { put :update, url_args }
 
-    context "fail" do
-      let(:url_args) { invalid_update_url_args }
+    include_examples "responds 301"
+    include_examples "resource has no errors"
+    include_examples "resource has expected attributes"
+  end
+end
 
-      before { post :update, url_args }
+RSpec.shared_examples "PUT :update, fail" do
+  context "fail" do
+    let(:url_args) { invalid_update_url_args }
 
-      include_examples "responds 200"
-      include_examples "resource has errors"
-    end
+    before { post :update, url_args }
+
+    include_examples "responds 200"
+    include_examples "resource has errors"
   end
 end
